f7478acbe1c4dd6835754a3a3da2f6a276254655
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-10-06 21:04:29 -0300

    finalizando_sistema_formulario_de_cadastro_utilizando_'api'_que_permite_abrir_um_arquivo_exitente_ou_salvar_um_cadastro_atual_criando_funcao_de_'salvar'

--- javascript/web_api/funcoes.js
+++ javascript/web_api/funcoes.js
@@ -13,3 +13,19 @@ function showFile(input) {
     }
     fr.readAsText(file);        // 'readAsText' lê o arquivo
 }
+
+function salvar() {
+    let templink = document.createElement("a");         // 'link temporario' para fazer o dowload do arquivo que será salvo
+    let dadosSalvar = {     // objeto 'json' que será salvo
+        "nome":document.getElementById("usuario_nome").value,
+        "cep":document.getElementById("endereco_cep").value,
+        "rua":document.getElementById("endereco_rua").value,
+        "bairro":document.getElementById("endereco_bairro").value,
+        "cidade":document.getElementById("endereco_cidade").value,
+        "estado":document.getElementById("endereco_estado").value
+    }
+    let meu_arquivo = new Blob([JSON.stringify(dadosSalvar)], { type: "text/plain" });        // 'Blob' é um objeto, uma representação de arquivo, função 'JSON.stringify' transforma o objeto em 'string'
+    templink.setAttribute("href", URL.createObjectURL(meu_arquivo)); // criando a URL do Blob
+    templink.setAttribute("download", "dados.json"); // atributo 'download' recebendo o nome do arquivo 'dados.json'
+    templink.click();       // dispara o dowload
+}
